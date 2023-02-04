d09719bb613882ff14a88123b5f4cb9a0a7520ef
Dilang <dilang@berkeley.edu>
2023-02-03 18:09:44 -0800

    Create classes

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// const cursor = document.querySelector('.cursor')
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width="852px";
+canvas.height="596px";
+
+// const background = new Image();
+// background.src = '../img/background.bmp';
+// background.onload = function(){
+//     ctx.drawImage(background,0,0, canvas.width, canvas.height);   
+// }
