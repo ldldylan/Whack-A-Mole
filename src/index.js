afc29da37eb8d393b5a44e3bf36bbd5b2e617d09
Dilang <dilang@berkeley.edu>
2023-02-07 14:53:02 -0800

    Update whack animation

--- src/index.js
+++ src/index.js
@@ -40,8 +40,8 @@ window.onload = function() {
     // ture => game has started; false => game is paused
     let gameState = true;
 
-    // holes position [{}{}{}{}{}{}{}]
-    let sevenPos = [
+    // seven positions for mole to show up [{}{}{}{}{}{}{}]
+    let sevenMolesPos = [
       {top: "11%", left: "5.5%"},
       {top: "8%", left: "24.5%"},
       {top: "11%", left: "44.5%"},
@@ -50,12 +50,18 @@ window.onload = function() {
       {top: "40%", left: "8.5%"},
       {top: "41%", left: "42.5%"}
     ];
-    // last hole index
-    let lastIndex = -1;
+
+    // seven positions for shovel to show up
+
+    // previous hole index
+    let prevIndex = -1;
     // timer for generating moles
     let moleTimer = null;
-    // mole show up animation timer
+    // mole shows up animation timer
     let upTimer = null;
+    // mole goes down 
+    let downTimer = null;
+   
 
     // 2. Start game after clicking the start button
     startButton.onclick = function(){
@@ -72,7 +78,7 @@ window.onload = function() {
     function timeReduce(){
         timer = setInterval(function (){
             timeboxWidth--;
-            timebox.style.width = timeboxWidth+"px";
+            timebox.style.width = timeboxWidth + "px";
             if (timeboxWidth <= 0){
                 clearInterval(timer);
                 // gamer over
@@ -116,56 +122,164 @@ window.onload = function() {
       let mole = document.createElement("img");
       // i. which random hole
       // generate a random index 0-7
-      let holeIndex = Math.floor(Math.random()*7);
+      mole.holeIndex = Math.floor(Math.random()*7);
       // ensure no repetitive hole position
-      while(holeIndex === lastIndex){
-        holeIndex = Math.floor(Math.random()*7);
+      while(mole.holeIndex === prevIndex){
+        mole.holeIndex = Math.floor(Math.random()*7);
       }
-      lastIndex = holeIndex;
-      // while()
-      // 7 holes locations
-      console.log(sevenPos[holeIndex])
+      prevIndex = mole.holeIndex;
+      // 7. holes locations
+      console.log(sevenMolesPos[mole.holeIndex])
       // assign hole pos to mole
-      mole.style.top = sevenPos[holeIndex].top
-      mole.style.left = sevenPos[holeIndex].left
+      mole.style.top = sevenMolesPos[mole.holeIndex].top
+      mole.style.left = sevenMolesPos[mole.holeIndex].left
       // add mole image
       // mole.src="../img/m1.png"
 
       // ii. mole or snake
-      let who = ''
+      mole.who = ''
       let randomNumber = Math.floor(Math.random()*10)
       if (randomNumber === 0) {
-        who = 's';
+        mole.who = 's';
       }
-      else if (randomNumber === 1){
-        who = 'f';
-      }
-      else who = 'm';
+      // else if (randomNumber === 1){
+      //   mole.who = 'f';
+      // }
+      else mole.who = 'm';
 
       box.appendChild(mole);
       // showing up
-      let upIndex = 0
+      let upIndex = 0;
       upTimer = setInterval(function(){
-        console.log(upIndex);
-        mole.src = "../img/"+who+upIndex+".png";
-        upIndex++;
-        if (upIndex > 1){
-          // setTimeout(function(){}, 500)
-          // mole.src = "../img/m1"+Math.floor(Math.random()*7)+".png";
-          clearInterval(upTimer);
-          upIndex = 0;
+        if (mole.who === 'm' || mole.who === 'f'){
+          mole.src = "../img/" + mole.who + upIndex+".png";
+          upIndex++;
+          if (mole.who==='m' && upIndex > 1){
+            setTimeout(function(){}, 80)
+            mole.mineIndex = Math.floor(Math.random()*7);
+            mole.src = "../img/m1" + mole.mineIndex + ".png";
+            clearInterval(upTimer);
+          }
+          else if(mole.who==='f' && upIndex > 1){
+            setTimeout(function(){}, 80)
+            mole.src = "../img/f2.png";
+            clearInterval(upTimer);
+          }
         }
-      }, 1000)
+      }, 80)
+
+      // Going down
+      let downIndex = 1 
+      if (mole.who==='m'){
+        whack(mole);
+        setTimeout(function(){}, 80);
+        // let moel stays for 1 sec
+        mole.out = setTimeout(function(){
+          downTimer = setInterval(function(){
+            mole.src = "../img/m" + downIndex + ".png";
+            downIndex --;
+            if (downIndex < 0){
+              clearInterval(downTimer);
+              clearInterval(mole.out);
+              // delete mole element from box
+              box.removeChild(mole);
+            }
+          }, 80)
+        }, 1000)
+      }
+      else if (mole.who==='f'){
+        downIndex = 2;
+        mole.out = setTimeout(function(){
+          downTimer = setInterval(function(){
+            mole.src = "../img/" + mole.who + downIndex + ".png";
+            downIndex --;
+            if (downIndex < 0){
+              clearInterval(downTimer);
+              clearInterval(mole.out);
+              // delete mole element from box
+              box.removeChild(mole);
+            }
+          }, 80)
+        }, 1000)
+        whack(mole);
+      }
     }
+
     // gennerate many moles
     function showMole() {
       moleTimer = setInterval(function(){
         addMole()
       }, 1000)
     }
-    // showMole()
-}
+    // 8. whack a mole
+    function whack(mole) {
+      mole.onclick = function() {
+        // close downTimer before whacking
+        // clearInterval(mole.out)
+        // clearInterval(downTimer)
 
+        whackAnimation(mole);
+        if (mole.who === 'm'){
+          
+          switch(mole.mineIndex) {
+            case 0:
+              current_score += 5
+              break;
+            
+          }
+          current_score += 10;
+        }
+        else if(mole.who === 'f'){
+          current_score -= 10;
+        }
+        score.innerHTML = current_score;
+      } 
+    }
+    // whack timer
+    let whackTimer = null;
+    let hitTimer = null;
+
+    async function whackAnimation(mole){
+      let whack = document.createElement("img")
+      whack.style.top = sevenMolesPos[mole.holeIndex].top
+      whack.style.left = sevenMolesPos[mole.holeIndex].left
+      box.appendChild(whack)
+
+      // mole.whack = setTimeout(function() {
+        mole.src = "../img/m1" + mole.mineIndex + ".png";
+        let whackIndex = 0; 
+        whackTimer = setInterval(function(){
+          console.log("whackIndex:" + whackIndex)
+            whack.src = "../img/w" + whackIndex + ".png";
+            whackIndex++;
+            if (whackIndex >= 2) {
+              clearInterval(whackTimer);
+              // box.removeChild(whack);
+            }
+          }, 250)
+          hitAnimation(whack)
+          let hitIndex = 0;
+          
+          // }, 2000)
+        }
+        
+    async function hitAnimation(whack) {
+      hitTimer = setInterval(function() {
+        console.log("hitIndex: " + hitIndex)
+        whack.src = "../img/h" + hitIndex + ".png";
+        hitIndex++;
+        // console.log("hitIndex2: " + hitIndex)
+        if (hitIndex >= 2){
+          clearInterval(hitTimer);
+          box.removeChild(whack);
+          clearInterval(mole.out);
+          clearInterval(downTimer);
+          box.removeChild(mole);
+        // clearInterval(mole.whack);
+      }
+    }, 200)
+    }
+}
 
 
 
